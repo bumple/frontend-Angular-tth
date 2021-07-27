91db7aa737d51c2a5e49f9c39799e106325ae663
Violet <DarkVioletFairy@github.com>
2021-07-27 22:18:26 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutsComponent} from "./components/layouts/master/layouts.component";
+import {LoginComponent} from "./components/layouts/pages/login/login.component";
+import {RegisterComponent} from "./components/layouts/pages/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutsComponent } from './components/layouts/master/layouts.component';
+import { NavbarComponent } from './components/layouts/master/navbar/navbar.component';
+import { SidebarComponent } from './components/layouts/master/sidebar/sidebar.component';
+import { LoginComponent } from './components/layouts/pages/login/login.component';
+import { RegisterComponent } from './components/layouts/pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutsComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
